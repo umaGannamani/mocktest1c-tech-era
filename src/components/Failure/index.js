b55c0a143ad674201a0aac1cd4ab8b094a51f6e7
uma <udayarekhacowdary96@gmail.com>
2024-05-20 05:52:13 +0000

    # mocktest1c-tech-era

---
+++ src/components/Failure/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+const Failure = props => {
+  const {onRetry} = props
+
+  const onClickRetry = () => {
+    onRetry()
+  }
+
+  return (
+    <>
+      <div className="failure-view-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png "
+          alt="failure view"
+          className="failure-img"
+        />
+        <h1 className="heading">Oops! Something Went Wrong</h1>
+        <p className="desc">
+          We cannot seem to find the page you are looking for.
+        </p>
+        <button type="button" className="retry-button" onClick={onClickRetry}>
+          Retry
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Failure
